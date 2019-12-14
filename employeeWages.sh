6e96ea931b2b3930a9729d00d1a0d6edd4b9e124
sanchez1023 <shubham1023@gmail.com>
2019-12-14 10:56:15 +0530

    employee present or absent

--- employeeWages.sh
+++ employeeWages.sh
@@ -1,3 +1,20 @@
 #! /bin/bash -x
 
 echo "employee wages"
+        
+        IS_FULL_TIME=1
+        
+        randomCheck=$((RANDOM % 3))
+       
+                if [ $IS_FULL_TIME -eq $randomCheck ]
+                then
+
+                        
+                        echo employee is present
+                else
+                        echo employe is absent
+                        
+
+                 fi
+                        
+
