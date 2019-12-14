3922c700c06b56e76572590640f7c437f9afb18b
sanchez1023 <shubham1023@gmail.com>
2019-12-14 11:23:18 +0530

    calulated employee salary

--- employeeWages.sh
+++ employeeWages.sh
@@ -1,16 +1,11 @@
 #!/bin/bash -x
 
   salary=0
-   IS_PART_TIME=1;
+  
 	IS_FULL_TIME=2;
    wagesperhour=10
    randomCheck=$((RANDOM % 3))
-   if [ $IS_PART_TIME -eq $randomCheck  ]
-      then 
-         employeehours=4
-
-         echo employee is partime
-      elif [ $IS_FULL_TIME -eq $randomCheck ]
+   if [ $IS_FULL_TIME -eq $randomCheck ]
       then
    
          employeehours=8
