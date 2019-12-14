054e58f796c0745190d87681ad0993385d89c1bb
sanchez1023 <shubham1023@gmail.com>
2019-12-14 11:35:21 +0530

    parttime and fulltime using switch case

--- employeeWages.sh
+++ employeeWages.sh
@@ -1,20 +1,22 @@
 #!/bin/bash -x
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+wagesperhour=10
+randomCheck=$((RANDOM % 3))
+               case $randomCheck in
+                   $IS_PART_TIME)     
+                         employeehours=4
+                         echo employee is partime;;
+                    $IS_FULL_TIME)
+                          employeehours=8
+                          echo employee is present;;
 
-  salary=0
-  
-	IS_FULL_TIME=2;
-   wagesperhour=10
-   randomCheck=$((RANDOM % 3))
-   if [ $IS_FULL_TIME -eq $randomCheck ]
-      then
-   
-         employeehours=8
-         echo employee is present
-      else
-         echo employe is absent
-         employeehours=0
+                                *)
+                           echo employe is absent
+                           employeehours=0;;
+                esac
 
-   fi
-         salary=$(( $wagesperhour*$employeehours))
-         echo $salary
+
+                     salary=$(( $wagesperhour*$employeehours))
+                      echo $salary
 
