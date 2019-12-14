2abd03b58efde4567d43c846a4afefe1a45980d1
sanchez1023 <shubham1023@gmail.com>
2019-12-14 15:46:58 +0530

    storing wages in array

--- employeeWages.sh
+++ employeeWages.sh
@@ -22,15 +22,22 @@ function  getWorkingHours(){
    echo $workHours
 }
 
+	calculateWages(){
+		local workHours=$1
+	echo $workHours	
+	wage=$(($workHours * $EMP_RATE_PER_HOUR))
+		echo $wage
+	}
 while [[ $totalWorkHours -lt $MAX_HOURS_IN_MONTH && $totalWorkDays -lt $NUM_WORKING_DAYS ]]
 do
    ((totalWorkDays++))
    workHours="$( getWorkingHours $((RANDOM%3)) )"
    totalWorkHours=$(( $totalWorkHours+$workHours ))
-
+	empDailyWages[$totalWorkDays]="$(calculateWages $workHours)"
 
 done 
-totalSalary=$(( $totalWorkHours*$EMP_RATE_PER_HOUR ))
-
+	totalSalary="$(calculateWages $workHours)"
+	
+	echo total salary ${empDailyWages[@]} 
 
 
