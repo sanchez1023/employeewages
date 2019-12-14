b145983852e7d3ca2f65b8706782ef0aaaecf70a
sanchez1023 <shubham1023@gmail.com>
2019-12-14 12:12:02 +0530

    calculated monthly wages using while loop

--- employeeWages.sh
+++ employeeWages.sh
@@ -1,32 +1,33 @@
+
 #!/bin/bash -x
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
 wagesperhour=10
 numberOfWorkingDays=30
 salary=0
+day=1
+while [ $day -lt $numberOfWorkingDays ]
+        do
+                randomCheck=$((RANDOM % 3))
 
-	for ((day=1; day<=$numberOfWorkingDays; day++))
-   	do
-      	randomCheck=$((RANDOM % 3))
-   
-   		echo day:$day
-  			 case $randomCheck in
-                  $IS_PART_TIME)    
-                     employeehours=4
-                     echo employee is partime;;
-                  $IS_FULL_TIME)
-                     employeehours=8
-                     echo employee is present;;
+        echo day:$day
+        case $randomCheck in
+                        $IS_PART_TIME)     
+                               employeehours=4
+                               echo employee is partime;;
+                        $IS_FULL_TIME)
+                               employeehours=8
+                               echo employee is present;;
 
-                  *)
-                     echo employe is absent
-                     employeehours=0;;
-         esac
-
-   
-               salary=$(($salary + $(( $wagesperhour*$employeehours))))
-               #salary+= $(($wagesperhour*$employeehours))
-               echo  salary is :$salary
-done 
+                                *)
+                               echo employe is absent
+                               employeehours=0;;
+        esac
 
 
+                  salary=$(($salary + $(( $wagesperhour*$employeehours))))
+                                        #salary+= $(($wagesperhour*$employeehours))
+                  echo  salary is :$salary
+                  day=$(($day+1))
+
+done
